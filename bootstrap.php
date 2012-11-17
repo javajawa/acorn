df9c73e5c98414a6e495d0d3853999c9a781a2ca
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-11-17 19:13:18 +0000

    Refactor path design

--- bootstrap.php
+++ bootstrap.php
@@ -8,20 +8,19 @@ namespace Acorn;
  */
 
 // Path configuration
-define('TOP_PATH', realpath(dirname(__FILE__) . '/..') . DIRECTORY_SEPARATOR);
-define('SYSTEM_PATH',  TOP_PATH    . 'acorn'       . DIRECTORY_SEPARATOR);
-define('PROJECT_PATH', TOP_PATH    . PROJECT_NAME  . DIRECTORY_SEPARATOR);
-
+define('ACORN_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 define('WWW_PATH', trim($_SERVER['HTTP_HOST'] . getenv('PUBLIC_PATH'), '/'));
-
 define('DEBUG', false !== getenv('DEBUG'));
 
+if (false === defined('PROJECT_PATH'))
+	die('Acorn: Fatal configuration error: PROJECT_PATH is not definted');
+
 // Load the Main utility class
-require(SYSTEM_PATH . 'Acorn.php');
+require(ACORN_PATH . 'Acorn.php');
 
 // Configure the class auto-loader
-Acorn::addClassPath(SYSTEM_PATH . '%c.php', '\Acorn\\');
-Acorn::addClassPath(SYSTEM_PATH . 'stubs/%c.php', '\Acorn\\');
+Acorn::addClassPath(ACORN_PATH . '%c.php', '\Acorn\\');
+Acorn::addClassPath(ACORN_PATH . 'stubs/%c.php', '\Acorn\\');
 spl_autoload_register('\Acorn\Acorn::loadClass');
 
 // Get the requested URL
